2ae7d3c3db2aa70622d4b2e4eff4bc94d59b9248
YcSoku <dsssyc02@gmail.com>
2024-11-02 10:38:05 +0800

    Make VkHydroCore as a library

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,37 @@
 #include <vector>
-#include "core/Core.h"
+#include <iostream>
+#include "HydroCore/Core.h"
 
 namespace NH = NextHydro;
 
+struct Data {
+    float value;
+};
+
 int main() {
 
-    auto core = new NH::Core();
+    fs::path jsonPath = RESOURCE_PATH / fs::path("run.hcs.json");
 
-    std::vector<float_t> testArray({2.0, 2.0, 3.0});
+    auto core = new NH::Core();
+    std::vector<float_t> outputArray = {};
+    std::vector<Data> testArray({
+        Data(0.0),
+        Data(1.0),
+        Data(2.0),
+    });
+
     auto buffer = core->createStorageBuffer(testArray);
-    auto computePipeline = core->createComputePipeline("test.comp.spv");
+//    auto computePipeline = core->createComputePipeline("test.comp.spv");
+    auto computePipeline = core->createComputePipeline(jsonPath.c_str());
     auto descriptorSet = core->createDescriptorSets(computePipeline, buffer);
     core->tick(computePipeline, descriptorSet);
     vkDeviceWaitIdle(core->device);
-    buffer.readData(testArray);
+    buffer.readData(outputArray);
+
+    std::cout << "\n==================== Computation Result ====================" << std::endl;
+    for (const auto& value : outputArray) {
+        std::cout << value << std::endl;
+    }
+
     return 0;
 }
